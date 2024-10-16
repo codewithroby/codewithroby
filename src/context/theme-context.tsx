3d97cbd1b7b4086027cbe29ee1bf0b0b606f331e
Roby Codes <robert.cfx@proton.me>
2024-10-16 23:37:18 +0200

    config(darkmode): added preferred device color mode.

--- src/context/theme-context.tsx
+++ src/context/theme-context.tsx
@@ -1,34 +1,49 @@
 "use client";
 
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
-type ThemeType = "light" | "dark";
+type Theme = "light" | "dark";
 
-type ThemeContextType = {
-  theme: ThemeType;
+type ThemeContext = {
+  theme: Theme;
   switchTheme: () => void;
 };
 
-type ThemeContextProviderType = {
+type ThemeContextProvider = {
   children: React.ReactNode;
 };
 
-const ThemeContext = createContext<ThemeContextType | null>(null);
+const ThemeContext = createContext<ThemeContext | null>(null);
 
-const ThemeContextProvider = ({ children }: ThemeContextProviderType) => {
-  const [theme, setTheme] = useState<ThemeType>("light");
+const ThemeContextProvider = ({ children }: ThemeContextProvider) => {
+  const [theme, setTheme] = useState<Theme>("light");
 
   const switchTheme = () => {
     if (theme === "light") {
       setTheme("dark");
-      window.localStorage.setItem("theme", "dark");
-      document.documentElement.classList.add("dark");
+      window.localStorage.setItem("theme", theme);
+      document.documentElement.classList.add(theme);
     } else {
       setTheme("light");
       window.localStorage.setItem("theme", "light");
       document.documentElement.classList.remove("dark");
     }
   };
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem("theme") as Theme | null;
+
+    if (localTheme) {
+      setTheme(localTheme);
+
+      if (localTheme === "dark") {
+        document.documentElement.classList.add("dark");
+      } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        setTheme("dark");
+        document.documentElement.classList.add("dark");
+      }
+    }
+  }, []);
 
   return (
     <ThemeContext.Provider
